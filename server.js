b0f007a7a0b5e884eb38ab84ae4cede67b21f058
SaadRimeh <saad.rimeh.01@gmail.com>
2026-07-29 21:25:43 +0300

    start

--- server.js
+++ server.js
@@ -1,42 +1,22 @@
-import express from 'express';
+
+import express from 'express'
 const app = express();
-app.use(express.json());
 const port = 5000;
 
+app.use(express.json());
 let students = [
-    { id: 1, name: "ebaa", age: 24 },
-    { id: 2, name: "ebaa1", age: 241 }
+    {id : 1 , name : "jehan" , age : 34}
 ];
 
-// جلب جميع الطلاب (GET)
-app.get('/api/students', (req, res) => {
+app.get('/api/students', (req,res)=>{
+    //famouse isuus 200 404 ...
     res.status(200).json({
-        success: true,
-        count: students.length,
-        data: students
-    });
-});
+        success : true,
+        count :students.length,
+        data : students
 
-// إضافة طالب جديد (POST)
-app.post('/api/students', (req, res) => {
-    const newstudent = req.body;
-    if (!newstudent.name || !newstudent.age) {
-        return res.status(400).json({
-            success: false,
-            message: "please provide name and age"
-        });
-    }
-    newstudent.id = students.length + 1;
-    students.push(newstudent);
-    res.status(201).json({
-        success: true,
-        message: "student added successfully",
-        data: newstudent
-    });
-});
-
-
-// تشغيل الخادم
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+    })
+})
+app.listen(5000, ()=>{
+    console.log("Server is running on http://localhost:" + port);
+})
